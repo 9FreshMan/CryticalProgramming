7cd58da32ffeffc7ba0dda54a7dc85a1844fa93b
Murderzy <anntarasiuk@gmail.com>
2022-09-12 23:27:37 +0300

    add tests

--- TestProject/AppTest.cs
+++ TestProject/AppTest.cs
@@ -37,6 +37,44 @@ namespace TestProject
             var exp = new ArgumentException("Invalid number, only one 'N'");
             Assert.AreEqual(exc.ParamName, exp.ParamName);
         }
+
+        //Ann's tests
+        [TestMethod]
+        public void RomanNumberNegativeDigitCrosTest()
+        {
+            //Parse test
+            Assert.AreEqual(-400, RomanNumber.Parse("-CD"), "-CD == -400");
+
+            Assert.AreEqual(-4, RomanNumber.Parse("-IV"), "-IV == -4");
+
+            Assert.AreEqual(-10, RomanNumber.Parse("-X"), "-X == -10");
+
+            Assert.AreEqual(-30, RomanNumber.Parse("-XXX"), "-XXX == -30");
+
+            Assert.AreEqual(-1999, RomanNumber.Parse("-MCMXCIX"), "-MCMXCIX == -1999");
+
+            //ToString test
+            RomanNumber romanNumber = new(-10);
+            Assert.AreEqual("-X", romanNumber.ToString());
+
+            romanNumber = new(-1);
+            Assert.AreEqual("-I", romanNumber.ToString());
+
+            romanNumber = new(-90);
+            Assert.AreEqual("-XC", romanNumber.ToString());
+
+            romanNumber = new(-55);
+            Assert.AreEqual("-LV", romanNumber.ToString());
+
+            romanNumber = new(-120);
+            Assert.AreEqual("-CXX", romanNumber.ToString());
+
+
+            //error
+            Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("1CMXCIX-"); });
+            Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("--1CMXCIX"); });
+            Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("--1CMXCIX-"); });
+        }
     }
 }
 
